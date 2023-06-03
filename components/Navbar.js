c90ea21da1595b2f1e8b5131a3da6b8b1ef738d0
Anusha-MG <anushamg511@gmail.com>
2023-06-03 17:29:39 +0530

    commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,20 +3,20 @@ import walletlogo from "../temp/walletlogo.png"
 
 const style = {
     wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
-    leftMenu: `flex gap-3`,
-    logo: `text-3xl text-white flex cursor-pointer mr-16`,
-    menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
-    rightMenu: `flex gap-3 items-center`,
-    userImageContainer: `mr-2`,
-    userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
-    loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
-    loginText: `ml-2`,
+    // leftMenu: `flex gap-3`,
+    // logo: `text-3xl text-white flex cursor-pointer mr-16`,
+    // menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
+    // rightMenu: `flex gap-3 items-center`,
+    // userImageContainer: `mr-2`,
+    // userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
+    // loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
+    // loginText: `ml-2`,
   }
 
 const Navbar = () => {
   return (
     <div className={style.wrapper}>
-        <div className={style.leftMenu}>
+        {/* <div className={style.leftMenu}>
             <div className={style.logo}>SafeGo</div>
             <div className={style.menuItem}>Ride</div>
             <div className={style.menuItem}>Drive</div>
@@ -30,7 +30,7 @@ const Navbar = () => {
                     <Image src={walletlogo} width={40} height={40} />
                 </div>
             </div>
-        </div>
+        </div> */}
     </div>
 
   )
