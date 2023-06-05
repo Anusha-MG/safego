4387cd79f6257af83025ba084737d8555c6a56e3
Anusha-MG <anushamg511@gmail.com>
2023-06-05 14:15:02 +0530

    commit

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -6,16 +6,15 @@ import Image from 'next/image'
 // import SolWagLogo from '../assets/SolWagLogo.png'
 
 const style = {
-  wrapper: `md:h-full md:w-52 h-16 w-full bg-primary text-white flex md:justify-around items-center px-4 fixed z-20 md:flex-col text-sm`,
-  leftMenu: `gap-3 md:flex-col justify-center flex`,
-  logo: `md:text-3xl flex cursor-pointer bg-gradient-to-r from-[#D900FA] via-[#00DBFD] to-[#00FF9D] text-transparent bg-clip-text w-full font-bold text-left ml-2`,
-  menuItem: `md:text-lg text-sm text-white font-medium flex items-center my-4 cursor-pointer hover:text-slate-500`,
-  rightMenu: `flex gap-3 items-center md:flex-col`,
+  wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
+  leftMenu: `flex gap-3`,
+  logo: `text-3xl text-white flex cursor-pointer mr-16`,
+  menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
+  rightMenu: `flex gap-3 items-center`,
   userImageContainer: `mr-2`,
   userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
   loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
   loginText: `ml-2`,
-  logoContainer: `flex items-center`
 }
 
 const Navbar = () => {
